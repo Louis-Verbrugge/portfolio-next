434869e608623306faf19981eb732dfa6a50fda0
Verbrugge <louvbg@gmail.com>
2024-04-12 19:24:35 +0200

    portfolio

--- app/barNav.tsx
+++ app/barNav.tsx
@@ -3,20 +3,20 @@ import "./barNav-style.css";
 import Link from 'next/link'
 
 const barNav = () => {
-    return (
-        <div className="navigation">
+  return (
+    <div className="navigation">
 
-          <div className="block-nav-active" id="menu">
+      <div className="block-nav-active" id="menu">
 
-            <Link href="#photoProfil">Profil</Link>
-            <Link href="#experience">Experiences</Link>
-            <Link href="#projet">Projets</Link>
-            <Link href="#langages">Langages</Link>
-            <Link target="_blank" href="#contact">CV</Link>
-            <Link href="#contact">Contact</Link>
-          </div>
-        </div>
-    );
-  };
-  
-  export default barNav;
+        <Link href="#photoProfil">Profil</Link>
+        <Link href="#experience">Experiences</Link>
+        <Link href="#projet">Projets</Link>
+        <Link href="#langages">Langages</Link>
+        <Link target="_blank" href="./CV Louis Verbrugge Etudiant informatique.pdf">CV</Link>
+        <Link href="#contact">Contact</Link>
+      </div>
+    </div>
+  );
+};
+
+export default barNav;
